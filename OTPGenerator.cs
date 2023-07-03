1cb2184b04cb7e56095fa5fa2a32b099267a18ea
916-Raiu-Madalin <raiumadalin@yahoo.com>
2023-07-04 02:16:58 +0300

    Add project files.

---
+++ OTPGenerator.cs
@@ -0,0 +1,27 @@
+﻿namespace OneTimePassword;
+
+public class OtpGenerator
+{
+    private const int PasswordValiditySeconds = 30;
+
+    public string GenerateOtp(string userId, DateTime dateTime)
+    {
+        var input = userId + dateTime.ToString("yyyyMMddHHmmss");
+        var hashedInput = GetHash(input);
+        var otp = hashedInput[..6]; // Use the first 6 characters as the OTP
+        return otp;
+    }
+
+    private string GetHash(string input)
+    {
+        var inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+        var hashBytes = System.Security.Cryptography.SHA256.HashData(inputBytes);
+        return BitConverter.ToString(hashBytes).Replace("-", "");
+    }
+
+    public bool IsOtpValid(string userId, DateTime dateTime, string otp)
+    {
+        var generatedOtp = GenerateOtp(userId, dateTime);
+        return otp == generatedOtp && dateTime.AddSeconds(PasswordValiditySeconds) >= DateTime.Now;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using OneTimePassword;
+
+var otpGenerator = new OtpGenerator();
+const string userId = "exampleUserId";
+var dateTime = DateTime.Now;
+var otp = otpGenerator.GenerateOtp(userId, dateTime);
+Console.WriteLine("Generated OTP: " + otp);
+
+Console.WriteLine("Please enter the OTP for verification:");
+while (true)
+{
+    var inputOtp = Console.ReadLine();
+    var isValid = inputOtp != null && otpGenerator.IsOtpValid(userId, dateTime, inputOtp);
+    Console.WriteLine("OTP is " + (isValid ? "valid" : "invalid"));
+}
